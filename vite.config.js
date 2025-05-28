7a241a3bf5fcdaa556bcf7af27271477f02d6250
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2025-05-28 12:03:04 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   root: './',
@@ -10,4 +11,14 @@ export default defineConfig({
   server: {
     port: 5173,
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'assets/dashboard/dashboard.html',
+          dest: 'assets/dashboard'
+        }
+      ]
+    })
+  ]
 });
